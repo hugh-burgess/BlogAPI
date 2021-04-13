fde0b6653cc14d9a6e1aed51502cc9480ed9d17f
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-13 18:01:41 +0200

    res.json(posts)

--- server/app.js
+++ server/app.js
@@ -17,19 +17,25 @@ app.use(express.json());
 /*
   Endpoint to handle GET requests to the root URI "/"
 */
-app.get("/", (req, res) => {
-  res.json({
-    "/posts": "read and create new posts",
-    "/posts/:id": "read, update and delete an individual post",
-    "second-post": "hello from the other side",
-    "another-post": "Hello from VSC",
-  });
+app.get("/posts", (req, res) => {
+  db.findAll()
+    .then((posts) => {
+      res.status(200);
+      res.json(posts);
+    })
+    .catch((error) => {
+      res.status(500);
+      res.send();
+    });
 });
 
 // this brings stuff from Postman into the Terminal here, then we tell the handler to put it somewhere and then use a frontend to render it in react!
 
 app.post("/users", (req, res) => {
-  console.log(req.body);
+  console.log(req.body); // request the body of the object into terminal
+  res.json({
+    hello: "world", //sending something back
+  });
 });
 
 /*
